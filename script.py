6cf6df817fc85396d408de4c1040694f4f2d5c55
Unknown <pieterjanwatteny@gmail.com>
2019-05-02 17:31:52 +0200

    fix ?

--- script.py
+++ script.py
@@ -3,15 +3,15 @@ import sys
 import Adafruit_DHT
 import I2C_LCD_driver
 from time import*
+
 mylcd = I2C_LCD_driver.lcd()
-
 print("Plantz monitor script running")
-
+mylcd.lcd.display_string('TEST',1);
 while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
-    mylcd.lcd.display_string('Temp: {0:0.1f} C'.format(temperature))
+
 
 
