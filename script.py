ea2e0c31f7567c891a69ae144f2541db5ffe0e77
Unknown <pieterjanwatteny@gmail.com>
2019-05-02 18:00:36 +0200

    ffs

--- script.py
+++ script.py
@@ -12,7 +12,7 @@ while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     mylcd.lcd_display_string('Temp: {0:0.1f} C'.format(temperature),1)
-    mylcd.lcd_display_string('Humidity: {1:0.1f} %'.format(humidity), 2)
+    mylcd.lcd_display_string('Hum: %'.format(humidity), 2)
 
     print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
