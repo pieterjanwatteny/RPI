cf66cedc2d7debebbef422047cd8ca787fe79aa1
Unknown <pieterjanwatteny@gmail.com>
2019-05-02 17:49:45 +0200

    more testing

--- lcdTest.py
+++ lcdTest.py
@@ -3,5 +3,6 @@ from time import *
 
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("I AM THE ONE", 1)
-mylcd.lcd_display_string("DONT WEIGH A TON", 2)
+mylcd.lcd_display_string("F*CK THIS WORLD", 1)
+
+
--- script.py
+++ script.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 import sys
-import I2C_LCD_driver
-from time import*
 import Adafruit_DHT
+import I2C_LCD_driver
+from time import *
 
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd = I2C_LCD_driver.lcd()
-print("Plantz monitor script running")
-mylcd.lcd.display_string('TEST',1);
+mylcd.lcd_display_string("F*CK THIS WORLD", 1)
+
 while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
