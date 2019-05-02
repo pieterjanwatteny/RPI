1f6d152331e2b45549cbaf2dfe84d6a139eeddeb
Unknown <pieterjanwatteny@gmail.com>
2019-05-02 17:46:52 +0200

    bsfdoubsfd

--- lcdTest.py
+++ lcdTest.py
@@ -3,4 +3,5 @@ from time import *
 
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("Hello World!", 1)
+mylcd.lcd_display_string("I AM THE ONE", 1)
+mylcd.lcd_display_string("DONT WEIGH A TON", 2)
