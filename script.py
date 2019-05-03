0610fe36c7ad4aeb72050324578cdfec68640b2f
Unknown <pieterjanwatteny@gmail.com>
2019-05-03 10:51:54 +0200

    Button+ LED

--- script.py
+++ script.py
@@ -3,11 +3,20 @@ import sys
 import Adafruit_DHT
 import I2C_LCD_driver
 import time
+import RPi.GPIO as GPIO
+
 mylcd = I2C_LCD_driver.lcd()
 
 mylcd.lcd_clear()
 
-mylcd.lcd_display_string("GATHER DATA", 1)
+mylcd.lcd_display_string("GATHERING DATA", 1)
+time.sleep(1)
+mylcd.lcd_display_string("PLEASE STAND BY..", 2)
+
+def buttonPres():
+    while True:
+        if GPIO.input(12)==GPIO.HIGH:
+            print("Button pressed")
 
 def read_print():
     while True:
@@ -18,4 +27,5 @@ def read_print():
         print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
 
+buttonPres()
 read_print()
