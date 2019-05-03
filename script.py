f6ce1eb24f470f00427ac6f14623a8baa2f86235
Unknown <pieterjanwatteny@gmail.com>
2019-05-03 10:59:07 +0200

    Second try

--- script.py
+++ script.py
@@ -10,7 +10,6 @@ mylcd = I2C_LCD_driver.lcd()
 mylcd.lcd_clear()
 
 mylcd.lcd_display_string("GATHERING DATA", 1)
-time.sleep(1)
 mylcd.lcd_display_string("PLEASE STAND BY..", 2)
 
 
@@ -18,10 +17,9 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-def buttonPres():
-    while True:
-        if GPIO.input(10)==GPIO.HIGH:
-            print("Button pressed")
+while True:
+    if GPIO.input(10)==GPIO.HIGH:
+        print("Button pressed")
 
 def read_print():
     while True:
@@ -32,5 +30,5 @@ def read_print():
         print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
 
-buttonPres()
 read_print()
+buttonPres()
