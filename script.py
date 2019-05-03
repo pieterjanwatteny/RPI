12283fdf319349f56467e667b82636908da90973
Unknown <pieterjanwatteny@gmail.com>
2019-05-03 10:55:43 +0200

    GPIO button reg + LED

--- script.py
+++ script.py
@@ -13,9 +13,14 @@ mylcd.lcd_display_string("GATHERING DATA", 1)
 time.sleep(1)
 mylcd.lcd_display_string("PLEASE STAND BY..", 2)
 
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
 def buttonPres():
     while True:
-        if GPIO.input(12)==GPIO.HIGH:
+        if GPIO.input(10)==GPIO.HIGH:
             print("Button pressed")
 
 def read_print():
