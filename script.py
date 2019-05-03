7a961febe7e9d967729e11e7468b132766ceca84
Unknown <pieterjanwatteny@gmail.com>
2019-05-03 11:30:36 +0200

    Kind off works

--- script.py
+++ script.py
@@ -23,7 +23,7 @@ GPIO.add_event_detect(10,GPIO.RISING,callback=buttonPress)
 def readPrint():
      while True:
          humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-         mylcd.lcd_display_string('Temp: {0:0.1f} C'.format(temperature,),1)
+         mylcd.lcd_display_string('Temp: {0:0.1f} °C'.format(temperature,),1)
          mylcd.lcd_display_string('Humidity: {1:0.1f} %'.format(temperature,humidity), 2)
          print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
 
