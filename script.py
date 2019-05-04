ee898ec1a4ba517be97d36882c79504504427597
Unknown <pieterjanwatteny@gmail.com>
2019-05-04 20:34:32 +0200

    Test post request

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import sys
 import Adafruit_DHT
 import I2C_LCD_driver
 import time
+import requests
 from time import sleep
 import RPi.GPIO as GPIO
 
@@ -11,6 +12,7 @@ mylcd.lcd_clear()
 
 mylcd.lcd_display_string("NOMNOM DATA", 1)
 mylcd.lcd_display_string("PLS STAND BY...", 2)
+
 
 
 def buttonPress(channel):
@@ -44,8 +46,10 @@ def readPrint():
         mylcd.lcd_display_string('Temp: {0:0.1f} C'.format(temperature,),1)
         mylcd.lcd_display_string('Humidity: {1:0.1f} %'.format(temperature,humidity), 2)
         print('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
+        r = requests.post("https://ibiome.herokuapp.com/api/data/", json={"Temp":temperature ,"Humid":humidity});
+        print(r.text)
 
-
+        
 def blink():
         while True: # Run forever
             GPIO.output(11, GPIO.HIGH)
